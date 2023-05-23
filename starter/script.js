5b1fc550c3a2ffb5fdb0871ccf5be948b24b5679
antoninogambino <112243266+antoninogambino@users.noreply.github.com>
2023-05-23 20:31:23 +0100

    adding function to retrieve weather data

--- starter/script.js
+++ starter/script.js
@@ -21,3 +21,15 @@ function createHistoryBtn(cityFormatted) {
   createHistoryBtn(cityFormatted);
   getWeatherData(cityFormatted);
 });
+
+// Function to retrieve weather data for the specified city
+function getWeatherData(cityFormatted) {
+  let geoQueryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityFormatted + "&appid=" + apiKey;
+
+  $.ajax({
+    url: geoQueryURL,
+    method: "GET"
+  }).then(function(response) {
+    let latitude = response[0].lat.toFixed(2);
+    let longitude = response[0].lon.toFixed(2);
+    let weatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
